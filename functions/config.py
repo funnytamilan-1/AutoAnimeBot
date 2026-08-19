a7f904be24b9f108c13f71be28a3a74164189e7f
Funnytamilan <ajmalitsme@gmail.com>
2026-08-19 14:17:16 +0530

    Update config.py

--- functions/config.py
+++ functions/config.py
@@ -40,7 +40,7 @@ class Var:
     BACKUP_CHANNEL = config("BACKUP_CHANNEL", default=-1002387577279, cast=int)
     MAIN_CHANNEL = config("MAIN_CHANNEL", default=-1002387577279, cast=int)
     LOG_CHANNEL = config("LOG_CHANNEL", default=-1003559364122, cast=int)
-    CLOUD_CHANNEL = config("CLOUD_CHANNEL", cast=int)
+    CLOUD_CHANNEL = config("CLOUD_CHANNEL", default=-1002387577279, cast=int)
     FORCESUB_CHANNEL = config("FORCESUB_CHANNEL", default=0, cast=int)
     OWNER = config("OWNER", default=8525952693, cast=int)
 
