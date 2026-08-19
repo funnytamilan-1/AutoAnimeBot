d4820a19288eda85dfe28367414b5d784f8508d7
Funnytamilan <ajmalitsme@gmail.com>
2026-08-19 14:32:57 +0530

    Update config.py

--- functions/config.py
+++ functions/config.py
@@ -26,7 +26,7 @@ class Var:
 
     # Telegram Credentials
 
-    API_ID = config("API_ID", cast=27806628)
+    API_ID = config("API_ID", cast=int)
     API_HASH = config("API_HASH", "25d88301e886b82826a525b7cf52e090")
     BOT_TOKEN = config("BOT_TOKEN", default="8761998102:AAGXEw-vBJniAPQAqSvUcYrsITZfbEt4_4Y")
     SESSION = config("SESSION", default=None)
